14a0536aed187a74a568fce5f7b8076b44ea47ce
KacperGielda <gkapi9@interia.eu>
2021-03-05 10:51:48 +0100

    full token auth working

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,11 @@
 const jwt = require('jsonwebtoken');
+// const mongoose = require('mongoose');
+const RefreshToken = require("../models/refreshToken.js");
 require("dotenv").config();
 
-
+const generateAccessToken = (userId)=>{
+    return jwt.sign({userId}, process.env.AccessTokenSecret, {expiresIn: '25s'});
+}
 
 module.exports = {
     authenticateToken(req, res, next){
@@ -14,14 +18,36 @@ module.exports = {
         next();
     });
     },
-    loginPOST(req, res){
+    login(req, res){
         const {userId} = req.body;
         // console.log(req);
-        const accessToken = jwt.sign({userId}, process.env.AccessTokenSecret, {expiresIn: '15s'});
+        // const accessToken = jwt.sign({userId}, process.env.AccessTokenSecret, {expiresIn: '15s'});
+        const accessToken = generateAccessToken(userId);
         const refreshToken = jwt.sign({userId}, process.env.RefreshTokenSecret);
-        res.json({accessToken, refreshToken});
+        RefreshToken.create({token: refreshToken}, (err)=>{
+            if(err && !refreshToken) return res.sendStatus(401);
+            return res.json({accessToken, refreshToken});
+            
+        })
     },
-    registerPOST(req, res){
+    register(req, res){
 
     },
+    async refreshToken(req, res){
+        const {refreshToken} = req.body;    
+        if(!refreshToken) return res.sendStatus(401);
+        if((await RefreshToken.find({token: refreshToken}).exec()).length == 0) return res.sendStatus(403);
+        jwt.verify(refreshToken, process.env.RefreshTokenSecret, (err, user)=>{
+            const accessToken = generateAccessToken(user.userId);
+            res.json({accessToken});
+        });
+    },
+    logout(req, res){
+        const {refreshToken} = req.body;
+        RefreshToken.deleteOne({token: refreshToken}, (err => {
+            if (err) return res.sendStatus(404);
+            else return res.sendStatus(204);
+        }))
+
+    }
 };
